3648b3ce11040eebd85b5ed6d09f515f9e84b295
Nunzio <nunziodebartolomeo03@gmail.com>
2025-01-24 16:13:53 +0100

    init

---
+++ src/main/java/co/develhope/gameez_progetto/GameezProgettoApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.gameez_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameezProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameezProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/develhope/gameez_progetto/GameezProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.gameez_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameezProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
