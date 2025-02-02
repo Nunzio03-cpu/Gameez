b7392a736550a2aea0418e901c30b67ab7fd375c
Biagio Grassia <biagiograssia95@gmail.com>
2025-02-02 15:36:04 +0100

    aggiunte package service e repository con annesse classi

--- src/main/java/co/develhope/gameez_progetto/controller/GameezController.java
+++ src/main/java/co/develhope/gameez_progetto/controller/GameezController.java
@@ -1,5 +1,6 @@
 package co.develhope.gameez_progetto.controller;
 
+import co.develhope.gameez_progetto.service.GameezService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
