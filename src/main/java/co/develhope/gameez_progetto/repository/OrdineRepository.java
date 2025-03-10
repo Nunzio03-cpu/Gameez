c951fde3df855c89a1d95c80d2201e592fd3dc59
Chibinini <pirainocostanza@gmail.com>
2025-03-10 22:54:54 +0100

    Implementata classe OrdineService e OrdineRepository

--- src/main/java/co/develhope/gameez_progetto/repository/OrdineRepository.java
+++ src/main/java/co/develhope/gameez_progetto/repository/OrdineRepository.java
@@ -1,7 +1,12 @@
 package co.develhope.gameez_progetto.repository;
 
 import co.develhope.gameez_progetto.entity.Ordine;
+import co.develhope.gameez_progetto.entity.Prodotto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrdineRepository extends JpaRepository<Ordine, Long> {
+
+    List<Ordine> findAllByStatusOrdineTrue();
 }
--- src/main/java/co/develhope/gameez_progetto/service/OrdineService.java
+++ src/main/java/co/develhope/gameez_progetto/service/OrdineService.java
@@ -1,11 +1,67 @@
 package co.develhope.gameez_progetto.service;
 
+import co.develhope.gameez_progetto.entity.Ordine;
 import co.develhope.gameez_progetto.repository.OrdineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrdineService {
     @Autowired
     private OrdineRepository ordineRepository;
+
+    public Ordine creaOrdine(Ordine ordine) {
+        return ordineRepository.save(ordine);
+    }
+
+    public List<Ordine> getAllOrdini() {
+        return ordineRepository.findAllByStatusOrdineTrue();
+    }
+
+    public Optional<Ordine> findById(Long id) {
+        Optional<Ordine> ordine = ordineRepository.findById(id);
+        if (ordine.isPresent()) {
+            return ordine;
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Ordine> updateOrdine(Long id, Ordine ordine) {
+        Optional<Ordine> optionalOrdine = ordineRepository.findById(id);
+        if (optionalOrdine.isPresent()) {
+            optionalOrdine.get().setCostoSpedizione(ordine.getCostoSpedizione());
+            Ordine ordine1 = ordineRepository.save(optionalOrdine.get());
+            return Optional.of(ordine1);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Ordine> deleteLogicalOrdine(Long id) {
+        Optional<Ordine> ordineOptional = ordineRepository.findById(id);
+        if (ordineOptional.isPresent()) {
+            Ordine ordine = ordineOptional.get();
+            ordine.setStatusOrdine(false);
+            ordineRepository.save(ordine);
+            return Optional.of(ordine);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Ordine> activeStatusOrdine(Long id) {
+        Optional<Ordine> ordineOptional = ordineRepository.findById(id);
+        if (ordineOptional.isPresent()) {
+            Ordine ordine = ordineOptional.get();
+            ordine.setStatusOrdine(true);
+            ordineRepository.save(ordine);
+            return Optional.of(ordine);
+        } else {
+            return Optional.empty();
+        }
+    }
 }
